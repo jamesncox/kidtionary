e90a8d8b6adc62a61131a1392adc498e805e3d4d
jamesncox <jamesncox@yahoo.com>
2020-05-22 11:44:32 -0500

    Add word reducer state

---
+++ src/reducers/word.js
@@ -0,0 +1,23 @@
+import {
+    SET_WORD,
+    CLEAR_WORD
+} from '../actionTypes'
+
+export default (state = { word: null, suggestions: null }, action) => {
+    switch (action.type) {
+        case SET_WORD:
+            let x = Object.values(action.payload)[0]
+            let y = Object.keys(x)
+            if (y.includes("meta")) {
+                return { ...state, word: action.payload, suggestions: null }
+            } else {
+                return { ...state, word: null, suggestions: action.payload }
+            }
+
+        case CLEAR_WORD:
+            return { ...state, word: null, suggestions: null }
+
+        default:
+            return state
+    }
+}
