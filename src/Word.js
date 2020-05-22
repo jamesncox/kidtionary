91740a4237ea3658cce57f2517cb19fb5b62933b
jamesncox <jamesncox@yahoo.com>
2020-05-22 11:47:12 -0500

    Add Word.js component

---
+++ src/Word.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Definition from './Definition'
+import { clearWord, clearQuery } from './actions/word'
+
+function Word(props) {
+
+    const handleClear = () => {
+        props.clearWord()
+        props.clearQuery()
+    }
+
+    const capitalize = (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    }
+
+    return (
+        <div className="word">
+            <h3>{capitalize(props.query)}</h3>
+            <button className="clear-button" onClick={handleClear}>Clear Info</button>
+            <Definition />
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    query: state.query.query
+})
+
+export default connect(mapStateToProps, { clearWord, clearQuery })(Word)
