64eaec321f874ec638cc01eef5311701c6611b26
jamesncox <jamesncox@yahoo.com>
2020-06-03 19:05:02 -0500

    Add audio playback to speech bubble / update styling

--- src/Audio.js
+++ src/Audio.js
@@ -1,33 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-function Audio(props) {
+class Audio extends Component {
 
-    const checkForObjectKeys = () => {
-        if (props.word) {
-            let x = Object.values(props.word)[0]
-            let y = Object.keys(x)
-            return y.includes("meta")
-        }
-    }
+    // checkForObjectKeys = () => {
+    //     if (this.props.word) {
+    //         let x = Object.values(this.props.word)[0]
+    //         let y = Object.keys(x)
+    //         return y.includes("meta")
+    //     }
+    // }
 
-    const playAudio = () => {
-        const audioEl = document.getElementsByClassName("audio-element")
+    // checkForMP3 = () => {
+    //     if (this.props.word) {
+    //         let audioRef = this.props.word[0].hwi.prs[0].sound.audio
+    //         if (audioRef === undefined) {
+    //             return true
+    //         }
+    //     }
+    // }
+
+    playAudio = () => {
+        // something needs to put in here to reset the element with each new word
+        const audioEl = document.getElementsByClassName("audio-element")[0]
+        console.log(audioEl)
         audioEl.play()
     }
 
-    if (props.suggestions && checkForObjectKeys === true) {
-        return (
-            null
-        )
-    } else if (props.word) {
-        return (
-            <p className="audio-symbol">
-                {'\uD83D\uDDE9'}
-            </p>
-        )
-    } else {
-        return null
+    render() {
+        const checkForObjectKeys = () => {
+            if (this.props.word) {
+                let x = Object.values(this.props.word)[0]
+                let y = Object.keys(x)
+                return y.includes("meta")
+            }
+        }
+
+        // not sure if checkForMP3 is actually working
+        const checkForMP3 = () => {
+            if (this.props.word) {
+                let audioRef = this.props.word[0].hwi.prs[0].sound.audio
+                if (audioRef === undefined) {
+                    return true
+                }
+            }
+        }
+
+        if (this.props.suggestions && checkForObjectKeys() === true) {
+            return (
+                null
+            )
+        } else if (this.props.word) {
+            // not correctly filtering out the undefined audioRef words
+            const audioRef = this.props.word[0].hwi.prs[0].sound.audio
+            return (
+                <p className="audio-symbol" onClick={() => this.playAudio()}>
+                    <audio className="audio-element">
+                        <source src={`https://media.merriam-webster.com/audio/prons/en/us/mp3/${audioRef.slice(0, 1)}/${audioRef}.mp3`}></source>
+                    </audio>
+                    {'\uD83D\uDDE9'}
+                </p>
+            )
+        } else {
+            return null
+        }
     }
 }
 
--- src/Word.js
+++ src/Word.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Definition from './Definition'
 import { clearEverything, clearQuery } from './actions/word'
-// import Audio from './Audio.js'
+import Audio from './Audio.js'
 
 function Word(props) {
 
@@ -15,9 +15,10 @@ function Word(props) {
         return str.toUpperCase();
     }
 
+    // Add <Audio /> to <h3> </h3> below to render the speech bubble
     return (
         <div className="word">
-            <h3>{capitalize(props.query)}</h3>
+            <h3>{capitalize(props.query)} </h3>
             <button className="clear-button" onClick={handleClear}>Clear Info</button>
             <Definition />
         </div>
