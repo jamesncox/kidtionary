5bf578d104bea5dade771dfaf3c9da49d8fb584b
jamesncox <jamesncox@yahoo.com>
2020-06-04 15:44:43 -0500

    Update Audio.js conditionals to exclude prefixes

--- src/Audio.js
+++ src/Audio.js
@@ -13,6 +13,7 @@ function Audio(props) {
         if (props.word) {
             let x = Object.values(props.word)[0]
             let y = Object.values(x)
+            debugger
 
             if (Object.keys(x).includes("history")) {
                 return Object.keys(y[1].prs[0]).includes("sound")
@@ -22,7 +23,7 @@ function Audio(props) {
                 return false
             } else if (Object.keys(x).includes("hom")) {
                 return Object.keys(y[2].prs[0]).includes("sound")
-            } else if (Object.values(y[1])[0] === "re-") {
+            } else if (props.word[0].fl === 'prefix') {
                 return false
             } else {
                 return Object.keys(y[1].prs[0]).includes("sound")
