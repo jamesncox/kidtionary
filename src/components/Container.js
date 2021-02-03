3f5a4111ae3c18fce8756b10b7cf868fb12c1f90
jamesncox <jamesncox@yahoo.com>
2021-02-03 14:40:22 -0600

    Move Container.js

---
+++ src/components/Container.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { getWord } from '../actions/word'
+import Word from '../Word'
+import Query from '../Query'
+
+function Container(props) {
+
+    return (
+        <div>
+            <Query />
+            <Word />
+        </div>
+    )
+}
+
+export default connect(null, { getWord })(Container)
